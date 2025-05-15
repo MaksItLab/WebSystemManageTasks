12faac63b5ab5145af6eab7b5c7907fd72067ea2
MaksItLab <maksim.ardintsev@mail.ru>
2025-05-15 17:13:23 +0300

    Добавлено описание для TaskRepository

--- WebSystemManageTasks/Repositories/TaskRepository.cs
+++ WebSystemManageTasks/Repositories/TaskRepository.cs
@@ -3,8 +3,16 @@ using WebSystemManageTasks.Interfaces.Repositories;
 
 namespace WebSystemManageTasks.Repositories
 {
+    /// <summary>
+    /// Репозиторий пользователя
+    /// </summary>
     public class TaskRepository : ITaskRepository
     {
+        /// <summary>
+        /// Метод добавления задачи в базу данных
+        /// </summary>
+        /// <param name="task">Задача</param>
+        /// <returns></returns>
         public Task AddAsync(ProjectTask task)
         {
             Data.ProjectTasks.Add(task);
@@ -12,6 +20,11 @@ namespace WebSystemManageTasks.Repositories
             return Task.CompletedTask;
         }
 
+        /// <summary>
+        /// Метод удаления задачи из базы данных
+        /// </summary>
+        /// <param name="task">Задача</param>
+        /// <returns></returns>
         public Task DeleteAsync(ProjectTask task)
         {
             Data.ProjectTasks.Remove(task);
@@ -19,6 +32,11 @@ namespace WebSystemManageTasks.Repositories
             return Task.CompletedTask;
         }
 
+        /// <summary>
+        /// Метод обновления данных о задаче в базе данных
+        /// </summary>
+        /// <param name="task">Задача</param>
+        /// <returns></returns>
         public Task UpdateAsync(ProjectTask task)
         {
             var oldTask = Data.ProjectTasks.FirstOrDefault(x => x.Id == task.Id);
@@ -28,6 +46,10 @@ namespace WebSystemManageTasks.Repositories
             return Task.CompletedTask;
         }
 
+        /// <summary>
+        /// Метод получения всех задач из базы данных
+        /// </summary>
+        /// <returns>Перечисление задач</returns>
         public Task<IEnumerable<ProjectTask>> GetAllAsync()
         {
             var tasks = Data.ProjectTasks.Select(x => x);
@@ -35,6 +57,11 @@ namespace WebSystemManageTasks.Repositories
             return Task.FromResult(tasks);
         }
 
+        /// <summary>
+        /// Метод получения задачи по идентификатору из базы данных
+        /// </summary>
+        /// <param name="id">Идентификатор</param>
+        /// <returns>Задача</returns>
         public Task<ProjectTask?> GetTaskById(Guid id)
         {
             var task = Data.ProjectTasks.FirstOrDefault(x => x.Id == id);
