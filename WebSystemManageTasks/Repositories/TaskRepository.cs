a7bc18eba853d610a5f1b48c803e5bf7bc74ba56
MaksItLab <maksim.ardintsev@mail.ru>
2025-06-03 14:05:38 +0300

    Изменены 2 репозитория под базу данных: User

--- WebSystemManageTasks/Repositories/TaskRepository.cs
+++ WebSystemManageTasks/Repositories/TaskRepository.cs
@@ -1,4 +1,6 @@
-﻿using WebSystemManageTasks.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using WebSystemManageTasks.Entities;
+using WebSystemManageTasks.Infrastructure.DatabaseContext;
 using WebSystemManageTasks.Interfaces.Repositories;
 
 namespace WebSystemManageTasks.Repositories
@@ -8,16 +10,25 @@ namespace WebSystemManageTasks.Repositories
     /// </summary>
     public class TaskRepository : ITaskRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="context">Контекс БД</param>
+        public TaskRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         /// <summary>
         /// Метод добавления задачи в базу данных
         /// </summary>
         /// <param name="task">Задача</param>
         /// <returns></returns>
-        public Task AddAsync(ProjectTask task)
+        public async Task AddAsync(ProjectTask task)
         {
-            Data.ProjectTasks.Add(task);
-
-            return Task.CompletedTask;
+            await _context.ProjectsTasks.AddAsync(task);
         }
 
         /// <summary>
@@ -25,11 +36,10 @@ namespace WebSystemManageTasks.Repositories
         /// </summary>
         /// <param name="task">Задача</param>
         /// <returns></returns>
-        public Task DeleteAsync(ProjectTask task)
+        public async Task DeleteAsync(ProjectTask task)
         {
-            Data.ProjectTasks.Remove(task);
-
-            return Task.CompletedTask;
+            _context.ProjectsTasks.Remove(task);
+            await _context.SaveChangesAsync();
         }
 
         /// <summary>
@@ -37,26 +47,20 @@ namespace WebSystemManageTasks.Repositories
         /// </summary>
         /// <param name="task">Задача</param>
         /// <returns></returns>
-        public Task UpdateAsync(ProjectTask task)
+        public async Task UpdateAsync(ProjectTask task)
         {
-            var oldTask = Data.ProjectTasks.FirstOrDefault(x => x.Id == task.Id);
-
-            Data.ProjectTasks.Remove(oldTask);
-            
-            Data.ProjectTasks.Add(task);
-
-            return Task.CompletedTask;
+            _context.ProjectsTasks.Update(task);
+            await _context.SaveChangesAsync();
         }
 
         /// <summary>
         /// Метод получения всех задач из базы данных
         /// </summary>
         /// <returns>Перечисление задач</returns>
-        public Task<IEnumerable<ProjectTask>> GetAllAsync()
+        public async Task<IEnumerable<ProjectTask>> GetAllAsync()
         {
-            var tasks = Data.ProjectTasks.Select(x => x);
-
-            return Task.FromResult(tasks);
+            var tasks = await _context.ProjectsTasks.ToListAsync();
+            return tasks;
         }
 
         /// <summary>
@@ -64,11 +68,11 @@ namespace WebSystemManageTasks.Repositories
         /// </summary>
         /// <param name="id">Идентификатор</param>
         /// <returns>Задача</returns>
-        public Task<ProjectTask?> GetTaskById(Guid id)
+        public async Task<ProjectTask?> GetTaskById(Guid id)
         {
-            var task = Data.ProjectTasks.FirstOrDefault(x => x.Id == id);
+            var task = await _context.ProjectsTasks.FindAsync(id);
 
-            return Task.FromResult(task);
+            return task;
         }
     }
 }
